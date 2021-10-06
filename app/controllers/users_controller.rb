54fbfcff33af335dc371461c6a3845bb6596e4a8
sihgo109 <89370531+sihgo109@users.noreply.github.com>
2021-10-06 12:08:28 +0900

    user-edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,6 +47,19 @@ class UsersController < ApplicationController
     @users = User.search(params[:keyword])
   end
 
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+
   private
 
   def user_params
